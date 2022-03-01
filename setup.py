37e48d6937d3d69fe94575848f655bd0ede10e4c
Андрей Маракулин <a.p.marakulin@gmail.com>
2022-03-01 22:57:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='epdetection',
-      version='1.5.9',  # 1.5.x reserved for neural networks
+      version='1.5.0',  # 1.5.x reserved for neural networks
       description='PCB components detection module for EyePoint P10.',
       long_description=long_description,
       long_description_content_type="text/markdown",
