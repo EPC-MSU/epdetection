dfd7232427f8ba71de1b560ac11f5a5c885d0726
timerke <timerke@mail.ru>
2026-03-12 13:25:13 +0300

    Removed FakeGuiConnector mention #123461

--- detection/detect.py
+++ detection/detect.py
@@ -71,7 +71,7 @@ def detect_elements(img: np.ndarray, gc=None, trh_prob: float = 0.7, trh_corr_mu
     img : np.array
         RGB uint8 array. Image for detection.
     gc : GuiConnector
-        GuiConnector or FakeGuiConnector - class for progressbars and signals.
+        Class for progressbars and signals.
     trh_prob : float
         Classifier threshold. Elements with a probability below the threshold are rejected. Can take values from 0 to 1.
     trh_corr_mult : float
@@ -109,7 +109,7 @@ def detect_label(img: np.ndarray, gc=None) -> List[Element]:
     img : np.array
         RGB uint8 array. Image for detection.
     gc : GuiConnector
-        GuiConnector or FakeGuiConnector - class for progressbars and signals.
+        Class for progressbars and signals.
 
     Returns
     -------
@@ -135,7 +135,7 @@ def detect_BGA(img: np.ndarray, gc=None, trh_prob: float = 0.7, trh_corr_mult: f
     img : np.array
         RGB uint8 array. Image for detection.
     gc : GuiConnector
-        GuiConnector or FakeGuiConnector - class for progressbars and signals.
+        Class for progressbars and signals.
     trh_prob : float
         Classifier threshold. Elements with a probability below the threshold are rejected. Can take values from 0 to 1.
     trh_corr_mult : float
@@ -173,7 +173,7 @@ def detect_BGA_params(img: np.ndarray, gc=None):
     img : np.array
         RGB uint8 array. Image for detection.
     gc : GuiConnector
-        GuiConnector or FakeGuiConnector - class for progressbars and signals.
+        Class for progressbars and signals.
 
     Returns
     -------
