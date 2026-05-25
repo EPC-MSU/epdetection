c1086f2b0830a3c055b7e6669502bbfdebfad7b9
timerke <timerke@mail.ru>
2026-05-25 17:32:52 +0300

    Version increment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 setup(name="epdetection",
-      version="1.0.10",
+      version="1.0.11",
       description="PCB components detection module",
       url="https://github.com/EPC-MSU/epdetection",
       author="EPC MSU",
